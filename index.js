564956547dd32942756c7b472ba8a1e9292c2474
Justus Dianga Okode <justusdiangaokode@gmail.com>
2022-11-30 14:16:49 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+      return `This Saturday, I want to ${activity}!`
+       
+}// code your solution here
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+
+}
+function wrapAdjective(param="*"){
+    return function inner(adjective="special"){
+        return `You are ${param}${adjective}${param}!`
+    }
+    
+ 
+       
+    
+    
+
+    }
+
